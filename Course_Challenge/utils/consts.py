1f23ea2f210c0af26f53093e9a6d8a7cb6ba2e32
Dvir613 <dvirbd613@gmail.com>
2024-08-05 00:21:31 +0300

    what necessary was added

--- Course_Challenge/utils/consts.py
+++ Course_Challenge/utils/consts.py
@@ -1,49 +1,33 @@
-# import pandas as pd
-#
-#
-# NUCLEOTIDES = ['A', 'C', 'G', 'T']
-#
-#
-# # Function to clean sequences (mainly from the quotation marks)
-# def clean_sequence(sequence):
-#     return ''.join([nt for nt in sequence if nt in NUCLEOTIDES])
-#
-#
-# # load necessary files:
-# PSSM_matrices = pd.ExcelFile("PSSM.xlsx")
-#
-# anti_SD_hybridization_energy = pd.read_excel("asd_hyb.xlsx")  # anti-Shine-Dalgarno hybridization energy
-# anti_SD_hybridization_energy.columns = ['Sequence', 'Energy']
-# anti_SD_hybridization_energy['Sequence'] = anti_SD_hybridization_energy['Sequence'].apply(clean_sequence)
-# # Create a dictionary from the DataFrame to map string to value
-# mapping_anti_SD_hybridization_energy = pd.Series(anti_SD_hybridization_energy['Energy'].values,
-#                                                  index=anti_SD_hybridization_energy['Sequence']).to_dict()
-
-
-
 import pandas as pd
+import numpy as np
 import os
 
 NUCLEOTIDES = ['A', 'C', 'G', 'T']
 
+
 # Function to clean sequences (mainly from the quotation marks)
 def clean_sequence(sequence):
     return ''.join([nt for nt in sequence if nt in NUCLEOTIDES])
+
 
 # Helper function to get the absolute path of the data files
 def get_data_file_path(filename):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(script_dir, filename)
 
+
 # Lazy loading mechanism for DataFrames
 PSSM_matrices = None
 mapping_anti_SD_hybridization_energy = None
+rnap_energy_mat = None
+
 
 def load_pssm_matrices():
     global PSSM_matrices
     if PSSM_matrices is None:
         PSSM_matrices = pd.ExcelFile(get_data_file_path("PSSM.xlsx"))
     return PSSM_matrices
+
 
 def load_anti_sd_hybridization_energy():
     global mapping_anti_SD_hybridization_energy
@@ -58,3 +42,19 @@ def load_anti_sd_hybridization_energy():
     return mapping_anti_SD_hybridization_energy
 
 
+def get_energy_matrix_for_rna_polymerase() -> pd.DataFrame:
+    """
+    from https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1002811
+        data link: https://doi.org/10.1371/journal.pcbi.1002811.s003
+        Energy matrix for RNAP in kT. Inferred from an experiment done in
+        TK310 with no supplemental cAMP (and hence, no CRP present in the
+        cells). The matrix covers base pairs [-41:-1] where 0 denotes the
+        transcription start site. Each row corresponds to a given position;
+        each column corresponds to a value for that base pair. The columns
+        are ordered [A,C,G,T].
+    """
+    promotor_strength_data = np.loadtxt(get_data_file_path('Energy matrix for RNAP.txt'))
+    return pd.DataFrame(promotor_strength_data, columns=list(NUCLEOTIDES))
+
+
+# rnap_energy_mat = get_energy_matrix_for_rna_polymeras()
